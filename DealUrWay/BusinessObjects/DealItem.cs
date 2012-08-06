127a2b8cd379f5aa1deddcf71b34f9ae77256a26
Suman Tedla <suman.tedla@gmail.com>
2012-08-06 16:19:31 -0700

    modified the business objects

--- DealUrWay/BusinessObjects/DealItem.cs
+++ DealUrWay/BusinessObjects/DealItem.cs
@@ -9,7 +9,18 @@ namespace DealUrWay.BusinessObjects
     {
         public string Description { get; set; }
         public DateTime CreatedDateTime { get; set; }
+        public DateTime LastUpdatedDateTime { get; set; }
         public string Title { get; set; }
         public Uri URI { get; set; }
+
+        /// <summary>
+        /// Html Content for the deal item
+        /// </summary>
+        public string Html { get; set; }
+
+        /// <summary>
+        /// Content if any for the deal item
+        /// </summary>
+        public string Content { get; set; }
     }
 }
