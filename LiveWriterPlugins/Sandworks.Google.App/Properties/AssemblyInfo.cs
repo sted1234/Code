6060a5d79a9a9017c714cd0dc6aa880be64a0e29
Suman Tedla <suman.tedla@gmail.com>
2012-08-06 13:57:39 -0700

    Reorganizing projects.

--- LiveWriterPlugins/Sandworks.Google.App/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Title of the current assembly.
-[assembly: AssemblyTitle("Sandworks.Google.App")]
--- LiveWriterPlugins/SearchDeals/Controllers/HomeController.cs
+++ LiveWriterPlugins/SearchDeals/Controllers/HomeController.cs
@@ -32,26 +32,11 @@ namespace SearchDeals.Controllers
 
         public ActionResult Search(string query)
         {
-            FeedWriter.PublishFeed();
             if (string.IsNullOrWhiteSpace(query))
                 return RedirectToAction("Index", "Home");
 
             IEnumerable<DealItem> deals = dealsProcessor.GetDeals(query);
             return View(deals);
-        }
-
-        public ActionResult SearchDeals(string query)
-        {
-            List<RawDealItem> results = new List<RawDealItem>();
-            using (ReaderService rdr = new ReaderService("affiliatested", "Sudha123", "Sandworks.Google.App"))
-            {
-                var resultsXml = rdr.Search(query, "20");
-                XNamespace xmlns = "http://www.w3.org/2005/Atom";
-                results = XElement.Parse(resultsXml).Elements(xmlns + "entry").Select(o => new RawDealItem(o, xmlns)).ToList<RawDealItem>();
-
-            }
-            return View(results.Distinct<RawDealItem>(new DealItemComparer()).ToList());
-
         }
 
 
