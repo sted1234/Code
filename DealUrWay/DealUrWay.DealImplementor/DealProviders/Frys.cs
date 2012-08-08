4e8b85fd5d9023033a4d2ae03e9e6783b1081d21
suman <affiliatested@gmail.com>
2012-08-07 23:25:39 -0700

    Added html agility pack. Added classes for frys and LivingSocial.cs

---
+++ DealUrWay/DealUrWay.DealImplementor/DealProviders/Frys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DealUrWay.DealImplementor.DealProviders
+{
+    public class Frys : DealProvider
+    {
+        public override BusinessObjects.IDealResponse GetDeals(BusinessObjects.IDealRequest request)
+        {
+            return base.GetDeals(request);
+        }
+    }
+}
---
+++ DealUrWay/DealUrWay.DealImplementor/DealProviders/LivingSocial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HtmlAgilityPack;
+
+namespace DealUrWay.DealImplementor
+{
+    public class LivingSocial : DealProvider
+    {
+        public override BusinessObjects.IDealResponse GetDeals(BusinessObjects.IDealRequest request)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("http://livingsocial.com");
+            return null;
+        }
+    }
+}
